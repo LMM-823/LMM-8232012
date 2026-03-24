3e4466a5345d808f97d9dd3f84e6c5378e14658e
LMM <liewyuhao9@gmail.com>
2026-03-24 22:03:23 +0800

    更新 Main.lua

--- Main.lua
+++ Main.lua
@@ -1,2 +1,2 @@
--- [[ 🌚刘某某脚本🌝 | 新仓库加载器 ]]
+-- [[ 🌚刘某某脚本🌝 | 新仓库加载器 (LMM-8232012) ]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LMM-823/LMM-8232012/main/Core.lua?t=" .. math.random(1, 999)))()
